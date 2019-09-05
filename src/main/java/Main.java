7d5d58a89895b3c7c4805b60ff57f7ca8ae79deb
Giselle Tavares <gitavares@hotmail.com>
2019-09-05 10:48:03 -0400

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[]args){
+
+        System.out.println("my git project");
+
+        System.out.println("what's your name?");
+        Scanner keyboard = new Scanner(System.in);
+        String personName = keyboard.nextLine();
+        System.out.println("Hello " + personName);
+
+
+    }
+
+}
